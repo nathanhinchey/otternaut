d59ba3bdfeed95b1dc5c179e4a21bf8f314ef850
Nathan Hinchey <nathanhinchey@gmail.com>
2015-04-11 18:34:15 -0400

    Create rails project.

---
+++ Otternaut/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Otternaut::Application.config.secret_key_base = '00a87c2e6d49da93a436992fa18e2088d9e844c940859f69339041a34d47e016b48f3fc2f8b93e4c26a2f0e97e844834b7ff2b1eedcad17975f740af3a51b937'
---
+++ Otternaut/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Otternaut::Application.config.session_store :cookie_store, key: '_Otternaut_session'
